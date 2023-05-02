84e9fd34028b7ebaa22499ca94a20955125ceb01
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2023-05-02 12:38:11 -0400

    Change to successful register message

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,22 @@
-import { useContext, useState, useEffect } from 'react'
-import { Alert } from 'react-native'
-import { useRoute, useNavigation } from '@react-navigation/native'
+import { useContext, useState, useEffect } from "react"
+import { Alert } from "react-native"
+import { useRoute, useNavigation } from "@react-navigation/native"
 
-import LoadingOverLay from '../components/Login/ui/LoadingOverlay'
-import { login } from '../util/auth'
-import AuthContent from '../components/Login/Auth/AuthContent'
-import { AuthContext } from '../store/auth-context'
+import LoadingOverLay from "../components/Login/ui/LoadingOverlay"
+import { login } from "../util/auth"
+import AuthContent from "../components/Login/Auth/AuthContent"
+import { AuthContext } from "../store/auth-context"
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
-  const [signUpSuccessMessage, setSignUpSuccessMessage] = useState('')
+  const [signUpSuccessMessage, setSignUpSuccessMessage] = useState("")
 
   route = useRoute()
   useEffect(() => {
     if (route.params?.signUpSuccess) {
-      setSignUpSuccessMessage('Sign up successful! Please log in.')
+      setSignUpSuccessMessage(
+        "Sign up successful! Please verify your email before you log in."
+      )
     }
   }, [route.params])
 
@@ -25,9 +27,9 @@ function LoginScreen() {
       const response = await login(username, password)
       //console.log(response.data._id)
       authCtx.authenticate(response.data.token, response.data._id)
-      if (response.data.error !== '') {
+      if (response.data.error !== "") {
         Alert.alert(
-          'Authentication failed!',
+          "Authentication failed!",
           `Could not log you in. ${response.data.error}`
         )
         setIsAuthenticating(false)
